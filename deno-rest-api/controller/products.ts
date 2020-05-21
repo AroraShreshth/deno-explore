9edc22d0d50f7761d1a67fc5c70ee55146a0614f
Shreshth Arora <sarora_be18@thapar.edu>
2020-05-22 03:08:54 +0530

    api/v1/products working

---
+++ deno-rest-api/controller/products.ts
@@ -0,0 +1,29 @@
+import { Product } from "../types.ts";
+let products: Product[] = [{
+  id: "1",
+  name: "Product One",
+  description: "This is product one",
+  price: 29.99,
+}, {
+  id: "2",
+  name: "Product Two",
+  description: "This is product two",
+  price: 39.99,
+}, {
+  id: "3",
+  name: "Product Three",
+  description: "This is product three",
+  price: 59.99,
+}];
+
+// @desc         GET ALL PRODUCTS
+//@ route  GET /api/v1/products
+
+const getProducts = ({ response }: { response: any }) => {
+  response.body = {
+    success: true,
+    data: products,
+  };
+};
+
+export { getProducts };
---
+++ deno-rest-api/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+import { getProducts } from "./controller/products.ts";
+
+const router = new Router();
+
+router.get("/api/v1/products", getProducts);
+
+export default router;
